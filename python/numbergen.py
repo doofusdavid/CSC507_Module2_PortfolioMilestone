9a214ed9dc4043a82ca9a9b6d798172aff768018
David Edwards <david.edwards@gmail.com>
2021-05-21 11:31:29 -0600

    initial in.

---
+++ python/numbergen.py
@@ -0,0 +1,6 @@
+from numpy import random
+
+numbers = random.randint(0, high=32767, size=1000)
+str_numbers = [str(int) for int in numbers]
+with open('file2.txt', 'w') as file:
+    file.write('\n'.join(str_numbers))
---
+++ shell/numbers.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in {0..999}
+do
+    echo $RANDOM >> file1.txt
+done
